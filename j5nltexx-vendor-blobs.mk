e547858e662cb8adb4e903e56ece8f41530e6b2b
nickverse <nikoskalomiris@gmail.com>
2016-12-17 20:02:29 +0200

    Add MM NFC HAL

--- j5nltexx-vendor-blobs.mk
+++ j5nltexx-vendor-blobs.mk
@@ -16,7 +16,7 @@
 
 PRODUCT_COPY_FILES += \
   vendor/samsung/j5nltexx/proprietary/vendor/firmware/libpn548ad_fw.so:system/vendor/firmware/libpn548ad_fw.so \
-  vendor/samsung/j5nltexx/proprietary/lib/hw/nfc_nci.msm8916.so:system/lib/hw/nfc_nci.msm8916.so \
+  vendor/samsung/j5nltexx/proprietary/lib/hw/nfc_nci.MSM8916.so:system/lib/hw/nfc_nci.MSM8916.so \
   vendor/samsung/j5nltexx/proprietary/lib/libnfc-nci.so:system/lib/libnfc-nci.so \
   vendor/samsung/j5nltexx/proprietary/bin/rild:system/bin/rild \
   vendor/samsung/j5nltexx/proprietary/vendor/lib/libril-qcril-hook-oem.so:system/vendor/lib/libril-qcril-hook-oem.so \
